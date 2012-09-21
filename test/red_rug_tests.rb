82a6c42ed765125268be246b7275dc6a59f43424
ren <ren@noemail.net>
2012-09-21 17:01:12 +0000

    added simple.md for testing, added from_file with tests, incremented version
    FossilOrigin-Name: 3f078cd9c034c864f7bac788ad058cb9fdf35a4e

--- test/red_rug_tests.rb
+++ test/red_rug_tests.rb
@@ -5,4 +5,12 @@ class RedRugTests < Test::Unit::TestCase
   def test_to_html_correct
     assert_equal "<h1>foo</h1>\n", RedRug.to_html('#foo')
   end
+
+  def test_from_file_default_correct
+    assert_equal "<h1>foo</h1>\n", RedRug.from_file('data/simple.md')
+  end
+
+  def test_from_file_to_html_correct
+    assert_equal "<h1>foo</h1>\n", RedRug.from_file('data/simple.md', 'html')
+  end
 end
