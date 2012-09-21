a3db4392e659aacd8be1b9901589d7f4a6a41907
ren <code@apotheon.net>
2012-09-21 17:01:12 +0000

    added simple.md for testing, added from_file with tests, incremented version

--- lib/red_rug.rb
+++ lib/red_rug.rb
@@ -1,10 +1,17 @@
 require 'redcarpet'
 
 module RedRug
-  def self.version; '0.1'; end
+  def self.version; '0.2'; end
 
-  def self.to_html(mstring)
+  def self.to_html(markdown_string)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    markdown.render(mstring)
+    markdown.render(markdown_string)
+  end
+
+  def self.from_file(filename, output_format='html')
+    case output_format
+    when 'html'
+      to_html(IO.read filename)
+    end
   end
 end
